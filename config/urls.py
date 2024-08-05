332c318a1c842dd17faa8018fa4737ec16d15d60
Alexander <a.biryukov164@gmail.com>
2024-08-05 17:19:57 +0400

    добавил приложения users и habits, добавил модель user

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from config.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('habits.urls', namespace='habits')),
+    path('users/', include('users.urls', namespace='users'))
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
