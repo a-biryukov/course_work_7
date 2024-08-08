132c1534587a1d79ab0ed0bf850f31a02be20a76
Alexander <a.biryukov164@gmail.com>
2024-08-08 18:29:55 +0400

    добавил урл для списка публичных привычек

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,8 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet
+from habits.views import HabitViewSet, HabitListAPIView
 
 app_name = HabitsConfig.name
 
@@ -9,5 +10,5 @@ router = SimpleRouter()
 router.register('', HabitViewSet)
 
 urlpatterns = [
-
+    path('list/', HabitListAPIView.as_view(), name='habit_list')
 ] + router.urls
