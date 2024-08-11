d6ac1afcab836f38669ada0ceb06744549ae9984
Alexander <a.biryukov164@gmail.com>
2024-08-11 16:22:07 +0400

    интеграция с телеграм

--- telegram/services.py
+++ telegram/services.py
@@ -8,6 +8,10 @@ from habits.models import Habit
 
 
 def send_telegram_message(habits: list) -> None:
+    """
+    Отправляет уведомления в телеграм
+    :param habits: Список объектов класса Habit
+    """
     for habit in habits:
         text = f'У Вас запланировано {habit.action} в {habit.place}'
         chat_id = habit.user.chat_id
@@ -21,6 +25,10 @@ def send_telegram_message(habits: list) -> None:
 
 
 def get_habits() -> list:
+    """
+    Получает объекты класса Habit, которым нужно отправить уведомления
+    :return: Список объектов класса Habit
+    """
     days_of_week = {
         1: 'tuesday',
         2: 'wednesday',
@@ -37,11 +45,12 @@ def get_habits() -> list:
     time_send_message_2 = (datetime_now + timedelta(minutes=8)).time()
 
     params = {
+        'user__chat_id__isnull': False,
         f'daysofweek__{days_of_week[day_of_week_today]}': True
     }
 
     habits = Habit.objects.filter(**params)
-
+    print(habits)
     habits = (
         habits.filter(runtime__time_1__gte=time_send_message_1, runtime__time_1__lte=time_send_message_2) |
         habits.filter(runtime__time_2__gte=time_send_message_1, runtime__time_2__lte=time_send_message_2) |
--- telegram/tasks.py
+++ telegram/tasks.py
@@ -5,5 +5,6 @@ from telegram.services import get_habits, send_telegram_message
 
 @shared_task
 def sending_notification():
+    """Отправляет уведомления в телеграмм"""
     habits = get_habits()
     send_telegram_message(habits)
