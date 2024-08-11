a7831058c19793bc8a52ea1cc5211bfc4eee6681
Alexander <a.biryukov164@gmail.com>
2024-08-12 00:58:14 +0400

    тестирование привычек

--- habits/serializer.py
+++ habits/serializer.py
@@ -44,36 +44,37 @@ class HabitSerializer(serializers.ModelSerializer):
         return habit
 
     def update(self, instance, validated_data):
-        days_of_week_data = validated_data.pop('daysofweek')
-        runtime_data = validated_data.pop('runtime')
-
-        days_of_week = instance.daysofweek
-        runtime = instance.runtime
-
         instance.action = validated_data.get('action', instance.action)
         instance.place = validated_data.get('place', instance.place)
         instance.duration = validated_data.get('duration', instance.duration)
-        instance.periodicity = validated_data.get('periodicity', instance.periodicity)
         instance.reward = validated_data.get('reward', instance.reward)
         instance.related_habit = validated_data.get('related_habit', instance.related_habit)
         instance.is_pleasurable = validated_data.get('is_pleasurable', instance.is_pleasurable)
         instance.is_public = validated_data.get('is_public', instance.is_public)
         instance.save()
 
-        days_of_week.monday = days_of_week_data.get('monday', days_of_week.monday)
-        days_of_week.tuesday = days_of_week_data.get('tuesday', days_of_week.tuesday)
-        days_of_week.wednesday = days_of_week_data.get('wednesday', days_of_week.wednesday)
-        days_of_week.thursday = days_of_week_data.get('thursday', days_of_week.thursday)
-        days_of_week.friday = days_of_week_data.get('friday', days_of_week.friday)
-        days_of_week.saturday = days_of_week_data.get('saturday', days_of_week.saturday)
-        days_of_week.sunday = days_of_week_data.get('sunday', days_of_week.sunday)
-        days_of_week.save()
+        if validated_data.get('daysofweek'):
+            days_of_week_data = validated_data.pop('daysofweek')
+            days_of_week = instance.daysofweek
 
-        runtime.time_1 = runtime_data.get('time_1', runtime.time_1)
-        runtime.time_2 = runtime_data.get('time_2', runtime.time_2)
-        runtime.time_3 = runtime_data.get('time_3', runtime.time_3)
-        runtime.time_4 = runtime_data.get('time_4', runtime.time_4)
-        runtime.time_4 = runtime_data.get('time_5', runtime.time_5)
-        runtime.save()
+            days_of_week.monday = days_of_week_data.get('monday', days_of_week.monday)
+            days_of_week.tuesday = days_of_week_data.get('tuesday', days_of_week.tuesday)
+            days_of_week.wednesday = days_of_week_data.get('wednesday', days_of_week.wednesday)
+            days_of_week.thursday = days_of_week_data.get('thursday', days_of_week.thursday)
+            days_of_week.friday = days_of_week_data.get('friday', days_of_week.friday)
+            days_of_week.saturday = days_of_week_data.get('saturday', days_of_week.saturday)
+            days_of_week.sunday = days_of_week_data.get('sunday', days_of_week.sunday)
+            days_of_week.save()
+
+        if validated_data.get('daysofweek'):
+            runtime_data = validated_data.pop('runtime')
+            runtime = instance.runtime
+
+            runtime.time_1 = runtime_data.get('time_1', runtime.time_1)
+            runtime.time_2 = runtime_data.get('time_2', runtime.time_2)
+            runtime.time_3 = runtime_data.get('time_3', runtime.time_3)
+            runtime.time_4 = runtime_data.get('time_4', runtime.time_4)
+            runtime.time_4 = runtime_data.get('time_5', runtime.time_5)
+            runtime.save()
 
         return instance
--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,89 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='user@mail.ru', password='123qwe')
+        self.habit = Habit.objects.create(
+                action='Чистить зубы',
+                place='Ванная комната',
+                duration='60',
+                is_pleasurable=False,
+                is_public=True,
+                user=self.user
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_habit_retrieve(self):
+        """Тестирование просмотра одной привычки"""
+        url = reverse('habits:habit-detail', args=(self.habit.pk,))
+        response = self.client.get(url)
+        result = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.get('action'), self.habit.action)
+
+    def test_habit_update(self):
+        """Тестирование обновления одной привычки"""
+        url = reverse('habits:habit-detail', args=(self.habit.pk,))
+        data = {
+            'action': 'Делать зарядку',
+            'place': 'Дома',
+            'duration': '100',
+            'is_pleasurable': False,
+        }
+        response = self.client.patch(url, data=data, format='json')
+        result = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.get('action'), 'Делать зарядку')
+        self.assertEqual(Habit.objects.all().count(), 1)
+
+    def test_habit_create(self):
+        """Тестирование создания привычки"""
+        data = {
+            'action': 'Делать зарядку',
+            'place': 'Дома',
+            'duration': '100',
+            'is_pleasurable': False,
+            'runtime': {'time_1': '07:00'},
+            'days_of_week': {'sunday': True}
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post('/habits/', data=data, format='json')
+        result = response.json()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(result.get('action'), 'Делать зарядку')
+        self.assertEqual(Habit.objects.all().count(), 2)
+
+    def test_habit_delete(self):
+        """Тестирование удаления привычки"""
+        url = reverse('habits:habit-detail', args=(self.habit.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Habit.objects.all().count(), 0)
+
+    def test_habit_list(self):
+        """Тестирование получения списка привычек"""
+        data = {'id': 4, 'runtime': None, 'days_of_week': None, 'action': 'Чистить зубы',
+                'place': 'Ванная комната', 'duration': '00:01:00', 'reward': None,
+                'is_pleasurable': False, 'is_public': True, 'user': 3, 'related_habit': None}
+        response = self.client.get('/habits/')
+        result = response.json().get('results')[0]
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(result, data)
+
+    def test_public_habit_list(self):
+        """Тестерование списка публичных привычек"""
+        data = {'id': 7, 'runtime': None, 'days_of_week': None, 'action': 'Чистить зубы',
+                'place': 'Ванная комната', 'duration': '00:01:00', 'reward': None,
+                'is_pleasurable': False, 'is_public': True, 'user': 6, 'related_habit': None}
+        response = self.client.get('/habits/list/')
+        result = response.json().get('results')[0]
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(result, data)
+
