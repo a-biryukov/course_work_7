2a7b7b06da50c8965a24911b16de1140763ca090
Alexander <a.biryukov164@gmail.com>
2024-08-12 15:02:57 +0400

    Настроил документацию

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,30 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from config.settings import MEDIA_URL, MEDIA_ROOT
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API Документация',
+        default_version='v1',
+        description='Документация по API "Атомные привычки"',
+        terms_of_service='',
+        contact=openapi.Contact(email='a.biryukov164@gmail.com'),
+        license=openapi.License(name=''),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('habits/', include('habits.urls', namespace='habits')),
     path('users/', include('users.urls', namespace='users')),
-    path('telegram/', include('telegram.urls', namespace='telegram'))
+    path('telegram/', include('telegram.urls', namespace='telegram')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
